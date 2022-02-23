0652c1078ec919e3aebe8118e8aebfe92beacdcc
Luis Guisado <lguisadom@gmail.com>
2022-02-23 17:28:49 -0500

    Crear endpoint actuator customizado

--- src/main/java/com/lagm/webflux/actuator/MyHealthEndpoint.java
+++ src/main/java/com/lagm/webflux/actuator/MyHealthEndpoint.java
@@ -4,10 +4,14 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 
 import org.springframework.boot.actuate.endpoint.annotation.DeleteOperation;
+import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
 import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
 import org.springframework.boot.actuate.endpoint.annotation.Selector;
 import org.springframework.boot.actuate.endpoint.annotation.WriteOperation;
+import org.springframework.stereotype.Component;
 
+@Component
+@Endpoint(id = "my-health")
 public class MyHealthEndpoint {
 	Map<String, String> healthMap = new LinkedHashMap<>();
 
